592a4834095224745749bfe98b2f27253db79f4a
dominic-wassef <domwassef@gmail.com>
2022-10-16 22:36:39 -0400

    adding

--- handlers/auth-handlers.go
+++ handlers/auth-handlers.go
@@ -1,7 +1,18 @@
 package handlers
 
-import "net/http"
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"myapp/data"
+	"net/http"
+	"time"
 
+	"github.com/dominic-wassef/ghostly/mailer"
+	"github.com/dominic-wassef/ghostly/urlsigner"
+)
+
+// UserLogin displays the login page
 func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
 	err := h.App.Render.Page(w, r, "login", nil, nil)
 	if err != nil {
@@ -9,6 +20,7 @@ func (h *Handlers) UserLogin(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// PostUserLogin attempts to log a user in
 func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -36,14 +48,135 @@ func (h *Handlers) PostUserLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// did the user check remember me?
+	if r.Form.Get("remember") == "remember" {
+		randomString := h.randomString(12)
+		hasher := sha256.New()
+		_, err := hasher.Write([]byte(randomString))
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+		rm := data.RememberToken{}
+		err = rm.InsertToken(user.ID, sha)
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		// set a cookie
+		expire := time.Now().Add(365 * 24 * 60 * 60 * time.Second)
+		cookie := http.Cookie{
+			Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+			Value:    fmt.Sprintf("%d|%s", user.ID, sha),
+			Path:     "/",
+			Expires:  expire,
+			HttpOnly: true,
+			Domain:   h.App.Session.Cookie.Domain,
+			MaxAge:   315350000,
+			Secure:   h.App.Session.Cookie.Secure,
+			SameSite: http.SameSiteStrictMode,
+		}
+		http.SetCookie(w, &cookie)
+		// save hash in session
+		h.App.Session.Put(r.Context(), "remember_token", sha)
+	}
+
 	h.App.Session.Put(r.Context(), "userID", user.ID)
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 
 }
 
+// Logout logs the user out, removes any remember me cookie, and deletes
+// remember token from the database, if it exists
 func (h *Handlers) Logout(w http.ResponseWriter, r *http.Request) {
+	// delete the remember token if it exists
+	if h.App.Session.Exists(r.Context(), "remember_token") {
+		rt := data.RememberToken{}
+		_ = rt.Delete(h.App.Session.GetString(r.Context(), "remember_token"))
+	}
+
+	// delete cookie
+	newCookie := http.Cookie{
+		Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-100 * time.Hour),
+		HttpOnly: true,
+		Domain:   h.App.Session.Cookie.Domain,
+		MaxAge:   -1,
+		Secure:   h.App.Session.Cookie.Secure,
+		SameSite: http.SameSiteStrictMode,
+	}
+	http.SetCookie(w, &newCookie)
+
 	h.App.Session.RenewToken(r.Context())
 	h.App.Session.Remove(r.Context(), "userID")
+	h.App.Session.Remove(r.Context(), "remember_token")
+	h.App.Session.Destroy(r.Context())
+	h.App.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
+}
+
+func (h *Handlers) Forgot(w http.ResponseWriter, r *http.Request) {
+	err := h.render(w, r, "forgot", nil, nil)
+	if err != nil {
+		h.App.ErrorLog.Println("Error rendering : ", err)
+		h.App.Error500(w, r)
+	}
+}
+
+func (h *Handlers) PostForgot(w http.ResponseWriter, r *http.Request) {
+	// parse form
+	err := r.ParseForm()
+	if err != nil {
+		h.App.ErrorStatus(w, http.StatusBadRequest)
+		return
+	}
+
+	// verify supplied email exists
+	var u *data.User
+	email := r.Form.Get("email")
+	u, err = u.GetByEmail(email)
+	if err != nil {
+		h.App.ErrorStatus(w, http.StatusBadRequest)
+		return
+	}
+
+	// create a link to password
+	link := fmt.Sprintf("%s/user/reset-password?email=%s", h.App.Server.URL, email)
+
+	// sign the link
+	sign := urlsigner.Signer{
+		Secret: []byte(h.App.EncryptionKey),
+	}
+	signedLink := sign.GenerateTokenFromString(link)
+	h.App.InfoLog.Println("Signed link is", signedLink)
+
+	// email the message
+	var data struct {
+		Link string
+	}
+	data.Link = signedLink
+	msg := mailer.Message{
+		To:       u.Email,
+		Subject:  "Password reset",
+		Template: "password-reset",
+		Data:     data,
+		From:     "admin@example.com",
+	}
+
+	h.App.Mail.Jobs <- msg
+	res := <-h.App.Mail.Results
+	if res.Error != nil {
+		h.App.ErrorStatus(w, http.StatusBadRequest)
+		return
+	}
+
+	// redirect the user
 	http.Redirect(w, r, "/users/login", http.StatusSeeOther)
 }
